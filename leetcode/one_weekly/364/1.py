3f42735751c7d90dd44fa56a901312278ba5c5ec
Lyui251 <126668428+Lyui251@users.noreply.github.com>
2023-09-25 16:07:01 +0800

    364周赛-1题

--- leetcode/one_weekly/364/1.py
+++ leetcode/one_weekly/364/1.py
@@ -1,1 +1,11 @@
+# 2864. 最大二进制奇数  [https://leetcode.cn/problems/maximum-odd-binary-number/description/]
+# 模拟题
 
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        n = len(s)
+        one_n = s.count('1')
+        if one_n == 1:
+            return '0' * (n - 1) + '1'
+        else:
+            return '1' * (one_n - 1) + '0' * (n - one_n) + '1'
