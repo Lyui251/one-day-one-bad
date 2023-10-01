08c8a60509e84cfd4e5beb690b52bc37d1cb8d38
1627147636@qq.com <1627147636@qq.com>
2023-10-01 21:47:15 +0800

    144双周赛-第1题

--- leetcode/two_weekly/114/1.py
+++ leetcode/two_weekly/114/1.py
@@ -1,1 +1,11 @@
+# 8038. 收集元素的最少操作次数 [https://leetcode.cn/problems/minimum-operations-to-collect-elements/description/]
 
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        mask, target = 0, (1 << (k + 1)) - 2
+        n = len(nums)
+        for i in range(n-1, -1, -1):
+            mask |= 1 << nums[i]
+            if mask & target == target:
+                return n - i
