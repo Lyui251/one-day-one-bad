5cd9aa2324b149df28f0be49a485539bd12a61d4
1627147636@qq.com <1627147636@qq.com>
2023-10-03 20:53:32 +0800

    状态机dp

--- leetcode/copypasta/dp.py
+++ leetcode/copypasta/dp.py
@@ -1,6 +1,14 @@
 # 树形dp
 # 待施工...
 """
-二叉树:  [337. 打家劫舍 III](https://leetcode.cn/problems/house-robber-iii/ "悬停显示")
-一般树:  [P1352 没有上司的舞会](https://www.luogu.com.cn/problem/P1352 "悬停显示")
+二叉树:  337. 打家劫舍 III:    https://leetcode.cn/problems/house-robber-iii/
+一般树:  P1352 没有上司的舞会:   https://www.luogu.com.cn/problem/P1352
+"""
+
+# 状态机dp (https://www.bilibili.com/video/BV1ho4y1W7QK/)
+"""
+714. 买卖股票的最佳时机含手续费: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
+122. 买卖股票的最佳时机 II:  https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/
+309. 买卖股票的最佳时机含冷冻期: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+123. 买卖股票的最佳时机 III: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iii/
 """
